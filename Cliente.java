9bd7614174b5ea8f713fb0a06c3d49ecfa454ccb
melivasr <anavasquez@estudiantec.cr>
2023-08-18 15:20:11 -0600

    Get and set class Envios

--- Cliente.java
+++ Cliente.java
@@ -106,12 +106,40 @@ class InterfazCliente extends JPanel{
     }
 
 		
-		
-		
 	private JTextField campo1, nick, ip;
 
     private JTextArea espaciochat;
 	
 	private JButton miboton;
 	
+}
+
+class Envios{
+    private String nick, ip, mensaje;
+
+    public String getNick() {
+        return nick;
+    }
+
+    public void setNick(String nick) {
+        this.nick = nick;
+    }
+
+    public String getIp() {
+        return ip;
+    }
+
+    public void setIp(String ip) {
+        this.ip = ip;
+    }
+
+    public String getMensaje() {
+        return mensaje;
+    }
+
+    public void setMensaje(String mensaje) {
+        this.mensaje = mensaje;
+    }
+
+
 }
